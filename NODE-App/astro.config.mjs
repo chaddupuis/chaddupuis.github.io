f98d70fc45410d830fc2c3be9f2f4c809edc97e4
Chad Dupuis <chad@yyhmsg.net>
2025-01-28 14:58:29 -0600

    reconfigure structure, deploy from NODE-App and local dev docker

--- NODE-App/astro.config.mjs
+++ NODE-App/astro.config.mjs
@@ -2,9 +2,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-
 import alpinejs from '@astrojs/alpinejs';
+import tailwind from '@astrojs/tailwind';
 
 // Supports weights 100-900
 import '@fontsource/big-shoulders-stencil-text';
@@ -16,7 +15,5 @@ import '@fontsource/material-icons-outlined';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://yyhmsg.net',
-    integrations: [mdx(), sitemap(), tailwind(), alpinejs()],
+    integrations: [mdx(), sitemap(), alpinejs(), tailwind()],
 });
-
-
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Chad Dupuis Portfolio Site
 
 astrojs, tailwindcss, alpinejs
+deploy to github pages with custom domain
 
+## Local Dev
+docker compose --env-file ./dev-api.env -f ./docker-compose-dev.yml up
+- enter container with 
+- docker exec -it chad-yyhmsg-front /bin/bash
+- localhost:4321 for dev
